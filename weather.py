c778e30503b3233df7cc73e9f6d68d53e027e835
Fer Torres <fertorresmx@gmail.com>
2022-01-28 02:37:03 -0600

    Add the HTTP Request

--- weather.py
+++ weather.py
@@ -1,7 +1,13 @@
 # weather.py
 
 import argparse
+import json
 from configparser import ConfigParser
+from urllib import parse, request
+
+# Check and update the API version here:
+BASE_WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather"
+
 
 def read_user_cli_args():
     """Handles the CLI user interactions.
@@ -23,6 +29,28 @@ def read_user_cli_args():
     )
     return parser.parse_args()
 
+
+def build_weather_query(city_input, imperial=False):
+    """Builds the URL for an API request to OpenWeather's weather API.
+
+    Args:
+        city_input (List[str]): Name of a city as collected by argparse
+        imperial (bool): Whether or not to use imperial units for temperature
+
+    Returns:
+        str: URL formatted for a call to OpenWeather's city name endpoint
+    """
+    api_key = _get_api_key()
+    city_name = " ".join(city_input)
+    url_encoded_city_name = parse.quote_plus(city_name)
+    units = "imperial" if imperial else "metric"
+    url = (
+        f"{BASE_WEATHER_API_URL}?q={url_encoded_city_name}"
+        f"&units={units}&appid={api_key}"
+    )
+    return url
+
+
 def _get_api_key():
     """Fetch the API key from your configuration file.
     
@@ -35,6 +63,23 @@ def _get_api_key():
     config.read("secrets.ini")
     return config["openweather"]["api_key"]
 
+
+def get_weather_data(query_url):
+    """Makes an API request to a URL and returns the data as a Python object.
+
+    Args:
+        query_url (str): URL formatted for OpenWeather's city name endpoint
+
+    Returns:
+        dict: Weather information for a specific city
+    """
+    response = request.urlopen(query_url)
+    data = response.read()
+    return json.loads(data)
+
+
 if __name__ == "__main__":
     user_args = read_user_cli_args()
-    print(user_args.city, user_args.imperial)
+    query_url = build_weather_query(user_args.city, user_args.imperial)
+    weather_data = get_weather_data(query_url)
+    print(weather_data)
