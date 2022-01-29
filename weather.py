d4e8d67e88499a6dd17a04dfff8e91e2ebcc0b21
Fer Torres <fertorresmx@gmail.com>
2022-01-29 02:44:21 -0600

    Build a display function

--- weather.py
+++ weather.py
@@ -3,7 +3,6 @@ import json
 import sys
 from configparser import ConfigParser
 from urllib import error, parse, request
-from pprint import pp
 
 # Check and update the API version here:
 BASE_WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather"
@@ -91,8 +90,26 @@ def get_weather_data(query_url):
         sys.exit("Couldn't read the server response.")
 
 
+def display_weather_info(weather_data, imperial=False):
+    """Prints formatted weather information about a city.
+
+    Args:
+        weather_data (dict): API response from OpenWeather by city name
+        imperial (bool): Whether or not to use imperial units for temperature
+
+    More information at https://openweathermap.org/current#name
+    """
+    city = weather_data["name"]
+    weather_description = weather_data["weather"][0]["description"]
+    temperature = weather_data["main"]["temp"]
+    print(f"{city}", end="")
+    print(f"\t{weather_description.capitalize()}", end=" ")
+    print(f"({temperature}°{'F' if imperial else 'C'})")
+
+
 if __name__ == "__main__":
     user_args = read_user_cli_args()
     query_url = build_weather_query(user_args.city, user_args.imperial)
     weather_data = get_weather_data(query_url)
-    pp(weather_data)
+
+    display_weather_info(weather_data, user_args.imperial)
